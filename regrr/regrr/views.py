f1e743969fddf954048a297f298a023335f69121
Данн Виталий <vdann@ya.ru>
2019-02-25 14:27:48 +0300

    * user_add.html - исправил ввод с мобильного браузера

--- regrr/regrr/views.py
+++ regrr/regrr/views.py
@@ -95,15 +95,15 @@ menus_user.extend(menus_all)
 def before_request():
 	path = request.path
 	if path.startswith('/static') or path in path_non_login:
-		print('before_request static:' + path)
+		# print('before_request static:' + path)
 		return
 	
 	user_id = session.get(SESSION_KEY_USER)
 	if not user_id:
-		print('before_request fail:' + path)
+		# print('before_request fail:' + path)
 		return redirect('/login')
 
-	print('before_request ok:' + path)
+	# print('before_request ok:' + path)
 
 #############################################################
 @app.route('/')
@@ -119,6 +119,7 @@ def index():
 	data['username'] = user_id.get('username')
 
 	user_role = user_id.get('role')
+
 
 	if user_role == db.UserRole.ADMIN:
 
@@ -212,16 +213,15 @@ def logout():
 @app.route("/profile")
 def profile():
 
-	user_id = session.get(SESSION_KEY_USER)
+	user_info = session.get(SESSION_KEY_USER)
 
 	title = ''
 	menus = []
 	data = {}
 	isAdmin = False
 
-	data['username'] = user_id.get('username')
-
-	user_role = user_id.get('role')
+	data['username'] = user_info.get('username')
+	user_role = user_info.get('role')
 
 	if user_role == db.UserRole.ADMIN:
 		isAdmin = True
@@ -240,6 +240,60 @@ def profile():
 	}
 
 	return render_template('profile.html', server = server)
+################################################################
+import werkzeug.exceptions as exceptions
+
+	if user_role == db.UserRole.ADMIN:
+		isAdmin = True
+		title = 'Профиль'
+		menus = menus_admin
+	else:
+		title = 'Профиль'
+		menus = menus_user
+
+	data = 'data = ' + json.dumps(data, indent=4,  ensure_ascii=False) + ';'
+	server = {
+		'title': title,
+		'isAdmin': isAdmin,
+		'data': data,
+		'menus': menus
+	}
+
+	return render_template('profile.html', server = server)
+
+
+	username = request.form.get('username')
+	password = username #request.form['password']
+	lastname = request.form.get('lastname')
+	firstname = request.form.get('firstname')
+	middlename = request.form.get('middlename')
+	# date_of_birth = request.form.get('date_of_birth')
+	position = request.form.get('position')
+	email = request.form.get('email')
+
+	user = db.User(username, password, db.UserRole.USER, lastname, firstname, middlename, position, email)
+
+	db_session = db.Session()
+	db_session.add(user)
+	db_session.commit()
+
+	return redirect('/')
+
+
+@app.route('/api/v1.0/test_username', methods=['POST'])
+def api_test_username():
+	if not request.json:
+		abort(400)
+
+	username = request.json.get('username')
+	if not username or username == '':
+		abort(400)
+
+	db_session = db.Session()
+	query = db_session.query(db.User).filter(
+		db.User.username.in_([username]))
+	result = query.first() == None
+	return jsonify({'result': result})
 
 
 ################################################################
--- regrr/runserver.py
+++ regrr/runserver.py
@@ -6,9 +6,10 @@ from os import environ
 from regrr import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+	HOST = environ.get('SERVER_HOST', 'localhost')
+	HOST = '0.0.0.0'
+	try:
+		PORT = int(environ.get('SERVER_PORT', '5555'))
+	except ValueError:
+		PORT = 5555
+	app.run(HOST, PORT)
