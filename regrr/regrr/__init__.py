81c0511a6bc83c07833473e40638984d6367aac8
Данн Виталий <vdann@ya.ru>
2019-02-26 13:28:37 +0300

    * переделал почту на gmail (yandex не работает на pythonanywhere)

--- regrr/regrr/__init__.py
+++ regrr/regrr/__init__.py
@@ -26,6 +26,7 @@ app.permanent_session_lifetime = timedelta(days=365)
 app.secret_key = '100' #os.urandom(12)
 
 #app.debug = True
+'''
 app.config['MAIL_SERVER'] = 'smtp.yandex.ru'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
@@ -33,6 +34,16 @@ app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'regrr.noreply@ya.ru'
 app.config['MAIL_DEFAULT_SENDER'] = 'regrr.noreply@ya.ru'
 app.config['MAIL_PASSWORD'] = '654321Q'
+'''
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+#app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'regrr.noreply@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'regrr.noreply@gmail.com'
+app.config['MAIL_PASSWORD'] = '654321Q!'
+
 
 # administrator list
 app.config['APP_ADMIN_MAILS'] = ['vdann@ya.ru']
