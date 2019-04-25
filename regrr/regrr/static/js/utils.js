83e4d45417ed88361e79450726959ba16add7985
Данн Виталий <vdann@ya.ru>
2019-04-25 13:27:30 +0300

    + добавил вывод возраста пациента

--- regrr/regrr/static/js/utils.js
+++ regrr/regrr/static/js/utils.js
@@ -1,6 +1,9 @@
 
 var Utils = (function () {
 
+	var export_ = {}
+
+	//------------------------------------
 	var regex_email = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/gm;
 	function isValidEmail(str) {
 		var isOk = str.search(regex_email) != -1;
@@ -11,6 +14,8 @@ var Utils = (function () {
 	var validators = {};
 	validators.isValidEmail = isValidEmail;
 
+	export_.validators = validators;
+
 
 	//------------------------------------
 	var icons = {};
@@ -18,8 +23,23 @@ var Utils = (function () {
 	icons.fail = 'fa fa-2x fa-exclamation-triangle clr-red';
 	icons.wait = 'fa fa-2x fa-refresh fa-spin clr-blue';
 
-	return {
-		validators : validators,
-		icons: icons
+	export_.icons = icons;
+
+	//------------------------------------
+	function nbsp(s) {
+		return s.replace(/ /g, '&nbsp;');
 	}
+
+	function b(s) {
+		return '<b>' + s + '</b>';
+	}
+
+	export_.html = {
+		nbsp: nbsp,
+		b: b
+	};
+
+	//------------------------------------
+
+	return export_;
 })();
